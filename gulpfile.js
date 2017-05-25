874096e272e7ee501fc55563a7506e740462dfae
Adam Morgan <atom.morgan@gmail.com>
2017-05-25 16:30:55 -0400

    Update gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var tsProject = ts.createProject('tsconfig.json');
 
-gulp.task('default', ['move-db-config', 'move-test-config'], function() {
+gulp.task('default', function() {
   return tsProject.src()
     .pipe(tsProject())
     .js.pipe(gulp.dest('built'));
